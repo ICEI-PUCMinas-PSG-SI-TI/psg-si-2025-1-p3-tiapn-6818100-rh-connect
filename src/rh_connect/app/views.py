88c2f5d23d662ee579a02ea66f2bebaf636299c9
Caio Grossi <caio.marcal@luizalabs.com>
2025-05-29 20:34:28 -0300

    terminei?

--- src/rh_connect/app/views.py
+++ src/rh_connect/app/views.py
@@ -58,10 +58,15 @@ class TodosChamadosView(LoginRequiredMixin, UserPassesTestMixin, ListView):
 class ObservacaoForm(forms.ModelForm):
     class Meta:
         model = Chamado
-        fields = ['observacao_rh']
+        fields = ['observacao_rh', 'status']
         widgets = {
             'observacao_rh': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
         }
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['observacao_rh'].required = False
 
 class ChamadoDetailView(LoginRequiredMixin, UserPassesTestMixin, FormMixin, DetailView):
     model = Chamado
